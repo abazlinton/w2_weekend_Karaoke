66874e7b7aacb8ebc9c122a0dfdbe7a414a5dcc8
Alex Bazlinton <abazlinton@gmail.com>
2016-10-08 12:27:29 +0100

    refactored Song for readability / SOC

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -12,12 +12,24 @@ class TestSong < Minitest::Test
 	end
 
 	def test_can_get_title
-		assert_equal("Heroes - 1999 Remastered Version", @song.name )
+		assert_equal("Heroes", @song.name[:orig])
 	end
 
 	def test_can_get_artist
 		assert_equal("David Bowie", @song.artist)
 	end
+
+	def test_can_download_sample
+
+	end
+
+	def test_failed_download_handled
+		
+	end
+
+	def test_failed_search_handled
+	end
+
 
 	def test_can_play_sample_mp3
 		@song.play_sample
@@ -26,11 +38,11 @@ class TestSong < Minitest::Test
 		assert_equal(true, @song_playing)
 	end
 
-	def test_can_get_array_of_5_most_popular_matches_for_query	
-		assert_equal(Array, @song.spotify_track_matches.class)
-		assert_equal(10, @song.spotify_track_matches.count)
-		puts @matching_songs
-	end
+	# def test_can_get_array_of_5_most_popular_matches_for_query	
+	# 	assert_equal(Array, @song.spotify_track_matches.class)
+	# 	assert_equal(10, @song.spotify_track_matches.count)
+	# 	puts @matching_songs
+	# end
 
 	def test_can_get_year
 		assert_equal("1977", @song.year)
