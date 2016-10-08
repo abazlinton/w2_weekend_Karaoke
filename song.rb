1bff47e67d6e1c56ab72ab53779195f4c8ab0281
Alex Bazlinton <abazlinton@gmail.com>
2016-10-08 12:28:06 +0100

    refactored Song for readability / SOC

--- song.rb
+++ song.rb
@@ -4,28 +4,31 @@ class Song
 	attr_reader :name, :spotify_id, :artist, :spotify_track_matches, :year
 
 	def initialize(params)	
-		setup_folders
-		@name_query = params[:name]
-		@spotify_track_matches = spotify_search(@name_query)
+		@name = {orig: params[:name], spotify: nil}
+		convert_song_to_spotify_track
+	end
+
+	def convert_song_to_spotify_track
+		@spotify_track_matches = spotify_search(name[:orig])
 		@top_hit = @spotify_track_matches[0]
 		@spotify_id = @top_hit.id
-		@name = @top_hit.name
+		@name[:spotify] = @top_hit.name
 		@artist = @top_hit.artists[0].name
-		#binding.pry
-		@preview_mp3_url = @top_hit.preview_url
 		@year = get_year()
+	end	
 		
+	def download_sample
+		setup_folders
+		@file = "../mp3/" + @spotify_id + ".mp3" 
+		if !File.exists?(@file)
+			curl_result = Curl::Easy.download(@top_hit.preview_url, @file)
+		end
 	end
 
 	def play_sample
-		@local_file_relative = "../mp3/" + @spotify_id + ".mp3" 
-		if !File.exists?(@local_file_relative)
-			curl_result = Curl::Easy.download(@preview_mp3_url, @local_file_relative)
-		end
-		fork{ exec 'afplay', @local_file_relative }
+		download_sample
+		fork{ exec 'afplay', @file }
 	end
-
-
 
 	def spotify_search(name)
 		return RSpotify::Track.search(name, limit:10, offset: 0, market: nil)
